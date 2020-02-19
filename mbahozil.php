6aaee69c62484eb5026ed27758a61b498ddb5111
Bang Ozil <60285156+BangOzil@users.noreply.github.com>
2020-02-19 14:30:29 +0700

    Update mbahozil.php

--- mbahozil.php
+++ mbahozil.php
@@ -88,7 +88,7 @@ echo color("green"," =================================== \n");
 						echo "\n".color("red","-] Message: ".$messageboba10);
 					}
 					goride:
-					echo "\n".color("yellow","!] Claim Voc AYOCOBANGOJEK");
+					echo "\n".color("yellow","!] Claim Voc AYOCOBAGOJEK");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
@@ -99,14 +99,14 @@ echo color("green"," =================================== \n");
 					$message1 = fetch_value($goride,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message1);
 							
-					echo "\n".color("yellow","!] Claim Voc GOFOODSANTUY19");
+					echo "\n".color("yellow","!] Claim Voc GOFOOD022120A");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTUY9"}');
+					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022120A"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
 					sleep(3);
